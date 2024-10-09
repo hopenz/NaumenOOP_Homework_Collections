e8c459a2306a59ca771c1bdc5505228d1888c40a
hopenz <petrushina_03@inbox.ru>
2024-10-09 15:12:33 +0500

    Done homework

--- src/main/java/ru/naumen/collection/task2/Task2.java
+++ src/main/java/ru/naumen/collection/task2/Task2.java
@@ -1,7 +1,6 @@
 package ru.naumen.collection.task2;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Дано:
@@ -36,7 +35,26 @@ public class Task2
      * Возвращает дубликаты пользователей, которые есть в обеих коллекциях
      */
     public static List<User> findDuplicates(Collection<User> collA, Collection<User> collB) {
-        // TODO реализовать метод
-        return null;
+        Set<User> setA = new HashSet<>(collA);
+        List<User> result = new ArrayList<>();
+        for (User user : collB){
+            if (setA.contains(user)){
+                result.add(user);
+            }
+        }
+        return result;
     }
+    /**
+     * Ответы на вопросы:
+     * 1. Для решения этой задачи я выбрала две коллекции - Hashset и Arraylist, так как
+     * hashset не допустает дубликатов и операция contains выполняется за О(1), а Arraylist
+     * позволяет добавлять элемент в конец за О(1).
+     *
+     * 2-3. Так как я перезаписываю коллекцию collA в hashset - в начале сложность О(n),
+     * где n - количество эл-тов в коллекции collA;
+     * Далее прохожусь по массиву в цикле - сложность O(m), где m - количество элементов в collB;
+     * Проверка принаблежности элемента и добавление элемента в конец - О(1);
+     * Таким образом, итоговая сложность алгоритма получается О(n+m).
+     *
+     */
 }
--- src/main/java/ru/naumen/collection/task3/WarAndPeace.java
+++ src/main/java/ru/naumen/collection/task3/WarAndPeace.java
@@ -1,6 +1,9 @@
 package ru.naumen.collection.task3;
 
 import java.nio.file.Path;
+import java.util.*;
+
+import static java.util.Map.Entry.comparingByValue;
 
 /**
  * <p>Написать консольное приложение, которое принимает на вход произвольный текстовый файл в формате txt.
@@ -19,10 +22,53 @@ public class WarAndPeace
             "Лев_Толстой_Война_и_мир_Том_1,_2,_3,_4_(UTF-8).txt");
 
     public static void main(String[] args) {
+
+        Map<String, Integer> mapOfWOrds = new LinkedHashMap<>();
+
         new WordParser(WAR_AND_PEACE_FILE_PATH)
                 .forEachWord(word -> {
-                    // TODO ваше действие над word
+                    if (mapOfWOrds.containsKey(word)) {
+                        mapOfWOrds.put(word, mapOfWOrds.get(word) + 1);
+                    } else {
+                        mapOfWOrds.put(word, 1);
+                    }
                 });
-        // TODO выполнить задачу
+
+        List<Map.Entry<String,Integer>> listOfWords = new ArrayList<>(mapOfWOrds.entrySet());
+        listOfWords.sort(Map.Entry.comparingByValue());
+
+        System.out.println("TOP 10 наиболее используемых слов: ");
+        for (int i = listOfWords.size() - 1; i > listOfWords.size() - 11; i-- ){
+            System.out.println(listOfWords.get(i).getKey() + " - " + listOfWords.get(i).getValue());
+        }
+
+        System.out.println("LAST 10 наименее используемых слов: ");
+        for (int i = 0; i < 10; i++ ){
+            System.out.println(listOfWords.get(i).getKey() + " - " + listOfWords.get(i).getValue());
+        }
     }
+    /**
+     * Ответы на вопросы:
+     * 1. Для данной задачи я выбрала LinkedHashMap, так как эта коллекция быстро итерируется и
+     * в данном случает хранит слово и его счетчик (сколько раз встречалось слово).
+     *
+     * Также в качестве второй коллекции я выбрала Arraylist, так как обращение к элементу по индексу
+     * требует O(1), а быстрая сортировка выполняется за О(nlog n).
+     *
+     * 2-3. Проход по каждому слову в файле требует О(n),
+     * а добавление/обновление его в LinkedHashMap требует O(1). Итого: О(n).
+     *
+     * Далее создается Arraylist, который содержит все записи из LinkedHashMap. Сложность О(n),
+     * где n - количество записей.
+     *
+     * Быстрая сортировка выполняется за О(nlog n).
+     *
+     * Дважды проход в цикле по листу, где по индексу достается map.entry, а из него сначала
+     * берется сначала ключ, а потом значение: О(20) -> O(1).
+     *(Тут подробнее будет: проход 1 раз по циклу, где 10 эл-тов - О(10) -> О(1);
+     *Обращение к элементу по индексу в Arraylist требует О(1))
+     *
+     * ИТОГО: О(2n + nlog n) -> O(nlog n)
+     *
+     */
 }
